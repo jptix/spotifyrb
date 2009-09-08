6bd15ad4eac959a959364727b2a567827cca1d47
jptix <xjptix@gmail.com>
2009-09-09 01:27:42 +0200

    Cleanup

--- spotify.rb
+++ spotify.rb
@@ -2,71 +2,69 @@ require "rubygems"
 require "ffi"
 
 module Spotify
-  module Lib
-    extend FFI::Library
+  extend FFI::Library
 
-    ffi_lib "./lib/libspotify.so"
+  ffi_lib "./lib/libspotify.so"
 
-    API_VERSION = 1
-    LINK_TYPES = [:invalid, :track, :album, :artist, :search, :playlist]
+  API_VERSION = 1
+  LINK_TYPES = [:invalid, :track, :album, :artist, :search, :playlist]
 
-    attach_function :sp_link_create_from_string, [:string                   ], :pointer
-    attach_function :sp_link_as_string,          [:pointer, :string, :int   ], :int
-    attach_function :sp_link_release,            [:pointer                  ], :void
-    attach_function :sp_session_init,            [:pointer, :pointer        ], :int
-    attach_function :sp_link_as_track,           [:pointer                  ], :pointer
-    attach_function :sp_link_type,               [:pointer                  ], :int
-    attach_function :sp_track_name,              [:pointer                  ], :string
-    attach_function :sp_session_login,           [:pointer, :string, :string], :int
-    attach_function :sp_session_process_events,  [:pointer, :pointer        ], :void
-    attach_function :sp_error_message,           [:int                      ], :string
-    attach_function :sp_track_num_artists,       [:pointer                  ], :int
-    attach_function :sp_artist_name,             [:pointer                  ], :string
-    attach_function :sp_track_album,             [:pointer                  ], :pointer
-    attach_function :sp_album_artist,            [:pointer                  ], :pointer
-    attach_function :sp_album_name,              [:pointer                  ], :string
-    attach_function :sp_album_year,              [:pointer                  ], :int
-    attach_function :sp_album_release,           [:pointer], :void
-    attach_function :sp_artist_release,          [:pointer], :void
-    attach_function :sp_session_user,            [:pointer], :pointer
-    attach_function :sp_user_display_name,       [:pointer], :string
-    attach_function :sp_user_canonical_name,     [:pointer], :string
-    attach_function :sp_user_is_loaded,          [:pointer], :uchar
+  attach_function :sp_album_artist,            [:pointer                  ], :pointer
+  attach_function :sp_album_name,              [:pointer                  ], :string
+  attach_function :sp_album_release,           [:pointer                  ], :void
+  attach_function :sp_album_year,              [:pointer                  ], :int
+  attach_function :sp_artist_name,             [:pointer                  ], :string
+  attach_function :sp_artist_release,          [:pointer                  ], :void
+  attach_function :sp_error_message,           [:int                      ], :string
+  attach_function :sp_link_as_string,          [:pointer, :string, :int   ], :int
+  attach_function :sp_link_as_track,           [:pointer                  ], :pointer
+  attach_function :sp_link_create_from_string, [:string                   ], :pointer
+  attach_function :sp_link_release,            [:pointer                  ], :void
+  attach_function :sp_link_type,               [:pointer                  ], :int
+  attach_function :sp_session_init,            [:pointer, :pointer        ], :int
+  attach_function :sp_session_login,           [:pointer, :string, :string], :int
+  attach_function :sp_session_process_events,  [:pointer, :pointer        ], :void
+  attach_function :sp_session_user,            [:pointer                  ], :pointer
+  attach_function :sp_track_album,             [:pointer                  ], :pointer
+  attach_function :sp_track_name,              [:pointer                  ], :string
+  attach_function :sp_track_num_artists,       [:pointer                  ], :int
+  attach_function :sp_user_canonical_name,     [:pointer                  ], :string
+  attach_function :sp_user_display_name,       [:pointer                  ], :string
+  attach_function :sp_user_is_loaded,          [:pointer                  ], :uchar
 
 
-    class SessionConfig < FFI::Struct
-      layout :api_version,          :int,
-             :cache_location,       :pointer,
-             :settings_location,    :pointer,
-             :application_key,      :pointer,
-             :application_key_size, :uint,
-             :user_agent,           :pointer,
-             :callbacks,            :pointer,
-             :userdata,             :pointer
-    end
+  class SessionConfig < FFI::Struct
+    layout :api_version,          :int,
+           :cache_location,       :pointer,
+           :settings_location,    :pointer,
+           :application_key,      :pointer,
+           :application_key_size, :uint,
+           :user_agent,           :pointer,
+           :callbacks,            :pointer,
+           :userdata,             :pointer
+  end
 
-    callback :logged_in,          [:pointer, :int                     ], :void
-    callback :logged_out,         [:pointer                           ], :void
-    callback :metadata_updated,   [:pointer                           ], :void
-    callback :connection_error,   [:pointer, :int                     ], :void
-    callback :message_to_user,    [:pointer, :string                  ], :void
-    callback :notify_main_thread, [:pointer                           ], :void
-    callback :music_delivery,     [:pointer, :pointer, :pointer, :int ], :int
-    callback :play_token_lost,    [:pointer                           ], :void
-    callback :log_message,        [:pointer, :string                  ], :void
+  callback :connection_error,   [:pointer, :int                     ], :void
+  callback :logged_in,          [:pointer, :int                     ], :void
+  callback :logged_out,         [:pointer                           ], :void
+  callback :log_message,        [:pointer, :string                  ], :void
+  callback :message_to_user,    [:pointer, :string                  ], :void
+  callback :metadata_updated,   [:pointer                           ], :void
+  callback :music_delivery,     [:pointer, :pointer, :pointer, :int ], :int
+  callback :notify_main_thread, [:pointer                           ], :void
+  callback :play_token_lost,    [:pointer                           ], :void
 
-    class SessionCallbacks < FFI::Struct
-      layout :logged_in,          :logged_in,
-             :logged_out,         :logged_out,
-             :metadata_updated,   :metadata_updated,
-             :connection_error,   :connection_error,
-             :message_to_user,    :message_to_user,
-             :notify_main_thread, :notify_main_thread,
-             :music_delivery,     :music_delivery,
-             :play_token_lost,    :play_token_lost,
-             :log_message,        :log_message
-    end
-  end # Lib
+  class SessionCallbacks < FFI::Struct
+    layout :logged_in,          :logged_in,
+           :logged_out,         :logged_out,
+           :metadata_updated,   :metadata_updated,
+           :connection_error,   :connection_error,
+           :message_to_user,    :message_to_user,
+           :notify_main_thread, :notify_main_thread,
+           :music_delivery,     :music_delivery,
+           :play_token_lost,    :play_token_lost,
+           :log_message,        :log_message
+  end
 
   class Client
 
@@ -88,8 +86,15 @@ module Spotify
     # callbacks
     #
 
-    [ :on_login, :on_logout, :on_metadata_updated, :on_connection_error, :on_message_to_user, :on_notification,
-      :on_music_delivery, :on_lost_play_token, :on_log_message ].each do |meth|
+    [ :on_login, 
+      :on_logout, 
+      :on_metadata_updated, 
+      :on_connection_error, 
+      :on_message_to_user, 
+      :on_notification,
+      :on_music_delivery, 
+      :on_lost_play_token, 
+      :on_log_message ].each do |meth|
         module_eval <<-RUBY
           def #{meth}(&blk)
             @callbacks[#{meth}] = blk
@@ -109,7 +114,7 @@ module Spotify
         create_session
       end
 
-      check_error Spotify::Lib.sp_session_login(@session_ptr, user, pass)
+      check_error sp_session_login(@session_ptr, user, pass)
     end
 
     #
@@ -117,7 +122,7 @@ module Spotify
     #
 
     def logout
-      check_error Lib.sp_session_logout(@session_ptr)
+      check_error sp_session_logout(@session_ptr)
     end
 
     #
@@ -125,8 +130,8 @@ module Spotify
     #
 
     def info_for(uri)
-      link_ptr = Lib.sp_link_create_from_string(uri)
-      type = Lib::LINK_TYPES[Lib.sp_link_type(link_ptr)]
+      link_ptr = sp_link_create_from_string(uri)
+      type = LINK_TYPES[sp_link_type(link_ptr)]
 
       raise "invalid uri" if type == :invalid || type.nil?
       info = { :type => type }
@@ -137,7 +142,7 @@ module Spotify
         raise "unknown type #{type.inspect}"
       end
 
-      Lib.sp_link_release(link_ptr)
+      sp_link_release(link_ptr)
       info
     end
 
@@ -149,7 +154,7 @@ module Spotify
       sleep_ptr = FFI::MemoryPointer.new :int
 
       loop do
-        Spotify::Lib.sp_session_process_events(@session_ptr, sleep_ptr)
+        sp_session_process_events(@session_ptr, sleep_ptr)
         sleep(sleep_ptr.read_int/1000)
       end
     end
@@ -159,9 +164,9 @@ module Spotify
     def create_config
       raise "must set Client#key_file=" unless @key_file
       key     = File.read(@key_file)
-      @config = Spotify::Lib::SessionConfig.new
+      @config = SessionConfig.new
 
-      @config[:api_version]          = Spotify::Lib::API_VERSION
+      @config[:api_version]          = API_VERSION
       @config[:cache_location]       = FFI::MemoryPointer.from_string(@cache_location)
       @config[:settings_location]    = FFI::MemoryPointer.from_string(@settings_location)
       @config[:application_key]      = FFI::MemoryPointer.from_string(key)
@@ -171,7 +176,8 @@ module Spotify
 
     def create_callbacks
       log :creating_callbacks
-      session_callbacks = Spotify::Lib::SessionCallbacks.new
+      session_callbacks = SessionCallbacks.new
+      
       session_callbacks[:logged_in]          = method(:logged_in).to_proc
       session_callbacks[:logged_out]         = method(:logged_out).to_proc
       session_callbacks[:metadata_updated]   = lambda { |*args| invoke_callback(:on_metadata_updated, *args) }
@@ -187,38 +193,41 @@ module Spotify
 
     def create_session
       session_ptr_ptr = FFI::MemoryPointer.new(4)
-      check_error Spotify::Lib.sp_session_init(@config, session_ptr_ptr)
+      check_error sp_session_init(@config, session_ptr_ptr)
 
       @session_ptr = session_ptr_ptr.get_pointer(0)
     end
 
     def track_info_for(link_ptr)
-      track_ptr = Lib.sp_link_as_track(link_ptr)
+      track_ptr = sp_link_as_track(link_ptr)
       raise TypeError, "not a track" if track_ptr.nil?
+      info = { 
+        :album => {}, 
+        :name => sp_track_name(track_ptr),
+        :artists => []
+      }
 
-      info = { :album => {} }
-      info[:name] = Lib.sp_track_name(track_ptr)
-      artist_count = Lib.sp_track_num_artists(track_ptr)
-
-
+      artist_count = sp_track_num_artists(track_ptr)
       if artist_count > 0
-        artist_ptrs = (0...artist_count).map { |idx| Lib.sp_track_artist(track_ptr, idx) }
-        info[:artists] = artist_ptrs.map { |ptr| Lib.sp_artist_name(ptr) }
-        aritst_ptrs.each { |ptr| Lib.sp_artist_release(ptr) }
+        artist_ptrs = (0...artist_count).map { |idx| sp_track_artist(track_ptr, idx) }
+        artist_ptrs.each do |ptr|
+          info[:artists] << sp_artist_name(ptr)
+          sp_artist_release(ptr)
+        end
       end
 
-      album_ptr = Lib.sp_track_album(track_ptr)
+      album_ptr = sp_track_album(track_ptr)
       if album_ptr && !album_ptr.null?
-        artist_ptr = Lib.sp_album_artist(album_ptr)
+        artist_ptr = sp_album_artist(album_ptr)
         if artist_ptr && !artist_ptr.null?
-          info[:album][:artist] = Lib.sp_artist_name(artist_ptr)
-          Lib.sp_artist_release(artist_ptr)
+          info[:album][:artist] = sp_artist_name(artist_ptr)
+          sp_artist_release(artist_ptr)
         end
 
-        info[:album][:name] = Lib.sp_album_name(album_ptr)
-        info[:album][:year] = Lib.sp_album_year(album_ptr)
+        info[:album][:name] = sp_album_name(album_ptr)
+        info[:album][:year] = sp_album_year(album_ptr)
 
-        Lib.sp_album_release(album_ptr)
+        sp_album_release(album_ptr)
       end
 
       info
@@ -227,8 +236,8 @@ module Spotify
     def logged_in(session, error)
       log :logged_in, session, error
       check_error error
-      user_ptr = Lib.sp_session_user(session)
-      log :logged_in_as, Lib.sp_user_is_loaded(user_ptr) != 0 ? Lib.sp_user_display_name(user_ptr) : Lib.sp_user_canonical_name(user_ptr)
+      user_ptr = sp_session_user(session)
+      log :logged_in_as, sp_user_is_loaded(user_ptr) != 0 ? sp_user_display_name(user_ptr) : sp_user_canonical_name(user_ptr)
 
       invoke_callback :on_login, session, error
     end
@@ -241,7 +250,7 @@ module Spotify
 
     def check_error(error_code)
       if error_code != 0
-        raise Error, Spotify::Lib.sp_error_message(error_code)
+        raise Error, sp_error_message(error_code)
       end
     end
 
